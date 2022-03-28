95db7def7dcb596f1ff23d5bae80ff61f8683f2e
Dane Lowe <dane.r.lowe@gmail.com>
2022-03-29 11:09:25 +1300

    Get full url from argument

--- healthcheck.go
+++ healthcheck.go
@@ -7,16 +7,13 @@ import (
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	var url string
-
-	if port != "" {
-		url = "http://localhost:" + port
-	} else {
-		url = "http://localhost"
+	healthCheckURL := "http://localhost"
+	if len(os.Args) > 2 {
+		fmt.Println("Must have a single argument")
+	} else if len(os.Args) == 2 {
+		healthCheckURL = os.Args[1]
 	}
-
-	_, err := http.Get(url)
+	_, err := http.Get(healthCheckURL)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
